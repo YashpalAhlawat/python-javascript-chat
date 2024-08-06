3acaa4a86bf11a89f6e130f37d28c2ab6420ea7b
Yashpal Ahlawat <yashpalahlawat8@gmail.com>
2024-08-06 21:48:00 +0530

    initial commit

---
+++ app_backend/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+import requests
+from  dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+class User(BaseModel):
+    username: str
+
+@app.get('/')
+def hello():
+    return "hello there!"
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    response = requests.put('https://api.chatengine.io/users/',
+        data={
+            "username": user.username,
+            "secret": user.username,
+            "first_name": user.username,
+        },
+        headers={ "Private-key": os.getenv('CHAT_ENGINE_PRIV_KEY') }
+    )
+    return response.json()
